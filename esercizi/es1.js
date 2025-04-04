f7b1cc2745bb0b9460b209d277d1114150c222ab
AngeloRaffaele <ralf9997@gmail.com>
2025-04-04 14:30:29 +0200

    es

--- esercizi/es1.js
+++ esercizi/es1.js
@@ -33,13 +33,13 @@ function numeriDoppi(...numeri) {
 
 function maggiorenni(...person) {
     person = [...persons]
-    return person.filter(person => person.age >= 18).sort((a, b) => a.age - b.age).find(person=>person.age ===true);
+    return person.filter(person => person.age >= 18).sort((a, b) => a.age - b.age).map(person=> person.age);
 }
 
 const adults = maggiorenni(persons)
 
 console.log("la somma dell'array è:", somma());
 console.log("I numeri sono:", clonePersona, " i numeri pari moltiplicati per 2 sono:", numeriPari());
-console.log("I numeri pari moltiplicati con il metodo foreach:", numeriDoppi());
+console.log("I numeri pari moltiplicati con il metodo forEach:", numeriDoppi());
 console.log(adults);
 
